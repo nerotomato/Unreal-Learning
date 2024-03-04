9034b2a0b7bfadd62953b194e4bd2ff81207c46e
nerotomato <guanhaowen@outlook.com>
2024-03-04 22:48:23 +0800

    add cin code

--- CodePractice/HelloWorld/HelloWorld.cpp
+++ CodePractice/HelloWorld/HelloWorld.cpp
@@ -1,11 +1,21 @@
 ﻿#include "HelloWorld.h"
 
 #include <iostream>
+#include <string>
 
+using namespace std;
 int add(int a, int b);
 
 int main(){
     std::cout << "hello,world!" << std::endl;
     std::cout << add(1,2) << std::endl;
+
+    string name;
+    int age;
+    float weight;
+//    cin >> name;
+//    cin >> age;
+    cin >> name >> age >>weight;
+    cout << "Hello,"<< name << " ,age:" << age << " ,weight: "<< weight << endl;
     return 0;
 }
